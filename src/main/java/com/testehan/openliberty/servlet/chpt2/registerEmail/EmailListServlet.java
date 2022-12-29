bd62711dc2c90f0f232b3c955048cdccd6eb4a43
DanTestehan <tdan89@yahoo.com>
2022-12-29 11:40:05 +0200

    Added register email functionality

---
+++ src/main/java/com/testehan/openliberty/servlet/chpt2/registerEmail/EmailListServlet.java
@@ -0,0 +1,51 @@
+package com.testehan.openliberty.servlet.chpt2.registerEmail;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class EmailListServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String url = "/index.html";
+
+        // get current action
+        String action = req.getParameter("action");
+        if (action == null) {
+            action = "join"; // default action
+        }
+
+        // perform action and set URL to appropriate page
+        if (action.equals("join")) {
+            url = "/index.html"; // the "join" page
+
+        } else if (action.equals("add")) {
+        // get parameters from the request
+            String firstName = req.getParameter("firstName");
+            String lastName = req.getParameter("lastName");
+            String email = req.getParameter("email");
+
+            // store data in User object and save User object in database
+            User user = new User(firstName, lastName, email);
+//            UserDB.insert(user);
+
+            // set User object in request object and set URL
+            req.setAttribute("user", user);
+            url = "jsp/thanks.jsp"; // the "thanks" page
+
+        }
+
+        // forward request and response objects to specified URL
+        getServletContext().getRequestDispatcher(url).forward(req, resp);
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       doPost(req,resp);
+    }
+}
