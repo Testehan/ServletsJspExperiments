a08ac5485d415086217bc98f04bdbdebaa9b0659
DanTestehan <tdan89@yahoo.com>
2023-02-01 16:52:54 +0200

    chpt 21 - JSF introduction

---
+++ src/main/java/com/testehan/openliberty/servlet/chpt21/EmailList.java
@@ -0,0 +1,53 @@
+package com.testehan.openliberty.servlet.chpt21;
+
+
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+import java.io.Serializable;
+
+
+@Named
+@RequestScoped
+public class EmailList implements Serializable {
+
+    private User user;
+    private String message;
+
+    public EmailList(){}
+
+    @PostConstruct
+    public void init(){
+        this.user = new User();
+        this.message = "<h3>Hiiii there !!!!</h3>";
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String addToEmailList() {
+
+        if ("dan@yahoo.com".equalsIgnoreCase(user.getEmail())) {
+            message = "This email address already exists. Please enter another email address";
+            user.setEmail("");
+            return "email";
+        } else {
+            message = "Thanks for registering !";
+            return "thanksForRegistering";
+        }
+    }
+}
---
+++ src/main/java/com/testehan/openliberty/servlet/chpt21/User.java
@@ -0,0 +1,106 @@
+package com.testehan.openliberty.servlet.chpt21;
+
+import java.util.List;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String email;
+    private String address;
+    private boolean termsAndConditionsAccepted;
+    private List<String> courses;
+    private String language;
+    private String gender;
+    private String salary;
+
+    public User(){}
+
+    public User(String firstName, String lastName, String password, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.email=email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean isTermsAndConditionsAccepted() {
+        return termsAndConditionsAccepted;
+    }
+
+    public void setTermsAndConditionsAccepted(boolean termsAndConditionsAccepted) {
+        this.termsAndConditionsAccepted = termsAndConditionsAccepted;
+    }
+
+    public List<String> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<String> courses) {
+        this.courses = courses;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+}
