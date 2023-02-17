e8ba4bab6855409eb2659d98af0f2c7446763b25
DanTestehan <tdan89@yahoo.com>
2023-02-17 18:12:54 +0200

    PrimeFaces part 13 - process attribute

---
+++ src/main/java/com/testehan/openliberty/servlet/primeFaces/ProcessBean.java
@@ -0,0 +1,34 @@
+package com.testehan.openliberty.servlet.primeFaces;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
+public class ProcessBean {
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void display(){
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Hello " + firstName + " " + lastName));
+    }
+}
